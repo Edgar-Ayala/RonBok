12d75f12a55633f54c2f791e109a12166c4b5449
RonanRaw <ayalalereaedgarpablo@gmail.com>
2024-09-16 11:04:46 -0400

    Agregando el nuevo proyecto

---
+++ RonBok/src/main/java/com/CEME/RonBok/RonBokApplication.java
@@ -0,0 +1,13 @@
+package com.CEME.RonBok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonBokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonBokApplication.class, args);
+	}
+
+}
