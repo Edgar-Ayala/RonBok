75b2b3e54e8fdbff09baeca6b732d724f2c03137
RonanRaw <ayalalereaedgarpablo@gmail.com>
2024-09-16 14:08:38 -0400

    Lo demas

---
+++ RonBok/src/test/java/com/CEME/RonBok/RonBokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CEME.RonBok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonBokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
